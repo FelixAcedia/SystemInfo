d8b314cb1e0c902951eb0e4130bf9868e0a6eae3
AcediavonFluxenberg <67912780+AcediavonFluxenberg@users.noreply.github.com>
2022-11-17 18:15:05 +0100

    Projektdateien hinzufügen.

---
+++ SystemInfo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Management;
+using System.Text;
+
+namespace SystemInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder processorInfo = new StringBuilder(string.Empty);
+            ManagementClass mgmntClass = new ManagementClass("Win32_OperatingSystem");
+            ManagementObjectCollection mgmntObj = mgmntClass.GetInstances();
+            PropertyDataCollection properties = mgmntClass.Properties;
+            foreach (ManagementObject obj in mgmntObj)
+            {
+                foreach (PropertyData property in properties)
+                {
+                    try
+                    {
+                        processorInfo.AppendLine(property.Name + ":  " +
+                            obj.Properties[property.Name].Value.ToString());
+                    }
+                    catch
+                    {
+                    }
+                }
+                processorInfo.AppendLine();
+            }
+            Console.WriteLine(processorInfo);
+            Console.ReadKey();
+        }
+    }
+}
